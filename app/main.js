3d620cea317b4bf69dbe904fb3d935c48fa37a78
Jonathan Groberg <jongroberg@gmail.com>
2022-07-07 18:45:26 -0400

    Create main.js

---
+++ app/main.js
@@ -0,0 +1,50 @@
+// @ts-nocheck
+'use strict';
+
+const electron = require('electron');
+
+const app = electron.app;
+
+// adds debug features like hotkeys for triggering dev tools and reload
+require('electron-debug')();
+
+// prevent window being garbage collected
+let mainWindow;
+
+function onClosed() {
+	// dereference the window
+	// for multiple windows store them in an array
+	mainWindow = null;
+}
+
+function createMainWindow() {
+	const win = new electron.BrowserWindow({
+		width: 1100,
+		minHeight: 300,
+	  minWidth: 380,
+		backgroundColor: "#282c34"
+	});
+
+	win.loadURL(`file://${__dirname}/dashboard.html`);
+	win.on('closed', onClosed);
+	win.toggleDevTools()
+
+	return win;
+}
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
+
+app.on('activate', () => {
+	if (!mainWindow) {
+		mainWindow = createMainWindow();
+	}
+});
+
+app.on('ready', () => {
+	mainWindow = createMainWindow();
+	mainWindow.setMenu(null);
+});
