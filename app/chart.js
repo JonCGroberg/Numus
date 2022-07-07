21141130d7c4b91fd25e9b350c50ae89d7f695f7
Jonathan Groberg <jongroberg@gmail.com>
2022-07-07 18:47:10 -0400

    Create chart.js

---
+++ app/chart.js
@@ -0,0 +1,12 @@
+const Chartist = require("chartist")
+var data = {
+  series: [5, 3, 4]
+};
+
+var sum = function(a, b) { return a + b };
+
+new Chartist.Pie('.ct-chart', data, {
+  labelInterpolationFnc: function(value) {
+    return Math.round(value / data.series.reduce(sum) * 100) + '%';
+  }
+});
